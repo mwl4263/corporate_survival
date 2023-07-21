0f74320470fe65db88c3f6d25781ac824597e595
Mason Lapine <60404755+mwl4263@users.noreply.github.com>
2023-07-21 11:03:03 -0500

    changes pushed

---
+++ abilities.py
@@ -0,0 +1,20 @@
+#Abilities
+Poetry = {
+    "speed_multiplier": 2
+}
+
+Asyncio = {
+    "Amount_multiplier": 2
+}
+
+Doritos = {
+    "health_multiplier": 2
+}
+
+Monster = {
+    "speed_multiplier": 3
+}
+
+Coffee = {
+    "damage_multiplier": 2
+}
---
+++ upgrades.py
@@ -0,0 +1,21 @@
+
+#Upgrades
+Pycharm = {
+    "size": 1,
+    "speed": 5,
+    "dps": 50,
+    "amount": 2
+    }
+Vs_code = {
+    "size": 2,
+    "speed": 2.5,
+    "dps": 25,
+    "amount": 4,
+    }
+
+Atom = {
+    "size": 1,
+    "speed": 10,
+    "dps": 5,
+    "amount": 50,
+    }
